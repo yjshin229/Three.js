6e83f5e7ed33c713d0ec17ec347d079551a2287b
Youngjin <yjshin229@gmail.com>
2024-04-13 16:18:30 -0400

    add orthographic camera

--- 02/basic/src/main.js
+++ 02/basic/src/main.js
@@ -19,17 +19,32 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   1000
+// );
+
+const orthographicCamera = new THREE.OrthographicCamera(
+  -(window.innerWidth / window.innerHeight), //left
+  window.innerWidth / window.innerHeight, //right
+  1, //top
+  -1, //bottom
   0.1,
   1000
 );
 
-camera.position.x = 2;
-camera.position.y = 3;
-camera.position.z = 5;
-scene.add(camera);
+// camera.position.x = 2;
+// camera.position.y = 3;
+// camera.position.z = 5;
+// scene.add(camera);
+orthographicCamera.position.x = 1;
+orthographicCamera.position.y = 2;
+orthographicCamera.position.z = 3;
+orthographicCamera.lookAt(0, 0, 0); // where the cube is rn
+
+scene.add(orthographicCamera);
 
 //BoxGeometry : the basic box
 const geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -43,4 +58,5 @@ scene.add(mesh);
 
 //then finally needs to be rendered using the renderer.
 //needs the scene and the camera
-renderer.render(scene, camera);
+// renderer.render(scene, camera);
+renderer.render(scene, orthographicCamera);
