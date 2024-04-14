cd71db167c685a895b74e940f38b7e1e2faf9859
Youngjin <yjshin229@gmail.com>
2024-04-14 00:28:09 -0400

    axis, grid helper

--- 03/utility/src/ex01.js
+++ 03/utility/src/ex01.js
@@ -1,64 +1,78 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 // ----- 주제: AxesHelper, GridHelper
 
 export default function example() {
-	// Renderer
-	const canvas = document.querySelector('#three-canvas');
-	const renderer = new THREE.WebGLRenderer({
-		canvas,
-		antialias: true
-	});
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+  // Renderer
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
 
-	// Scene
-	const scene = new THREE.Scene();
+  // Scene
+  const scene = new THREE.Scene();
 
-	// Camera
-	const camera = new THREE.PerspectiveCamera(
-		75,
-		window.innerWidth / window.innerHeight,
-		0.1,
-		1000
-	);
-	camera.position.z = 5;
-	scene.add(camera);
+  // Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.z = 5;
+  camera.position.x = 1;
+  camera.position.y = 1;
+  scene.add(camera);
 
-	const light = new THREE.DirectionalLight(0xffffff, 1);
-	light.position.x = 1;
-	light.position.z = 2;
-	scene.add(light);
+  const ambientLight = new THREE.AmbientLight("white", 0.5);
+  scene.add(ambientLight);
 
-	// Mesh
-	const geometry = new THREE.BoxGeometry(1, 1, 1);
-	const material = new THREE.MeshStandardMaterial({
-		color: 'seagreen'
-	});
-	const mesh = new THREE.Mesh(geometry, material);
-	scene.add(mesh);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
 
-	// 그리기
-	const clock = new THREE.Clock();
+  // Mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshStandardMaterial({
+    color: "seagreen",
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.x = 1;
+  scene.add(mesh);
 
-	function draw() {
-		const time = clock.getElapsedTime();
+  //AxisHelper
+  const axesHelper = new THREE.AxesHelper(3);
+  scene.add(axesHelper);
 
-		mesh.rotation.y = time;
+  //GridHelper
+  const gridHelper = new THREE.GridHelper(3);
+  scene.add(gridHelper);
 
-		renderer.render(scene, camera);
-		renderer.setAnimationLoop(draw);
-	}
+  // render
+  const clock = new THREE.Clock();
 
-	function setSize() {
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
-		renderer.setSize(window.innerWidth, window.innerHeight);
-		renderer.render(scene, camera);
-	}
+  function draw() {
+    const time = clock.getElapsedTime();
 
-	// 이벤트
-	window.addEventListener('resize', setSize);
+    mesh.rotation.y = time;
 
-	draw();
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener("resize", setSize);
+
+  draw();
 }
