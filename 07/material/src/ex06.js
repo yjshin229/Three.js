786f2efd9a0649e95e5fa9c0dafbb21269a3fdc6
Youngjin <yjshin229@gmail.com>
2024-04-15 03:44:56 -0400

    texture loader

---
+++ 07/material/src/ex06.js
@@ -0,0 +1,90 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+// ----- Texture Image Load
+
+export default function example() {
+  //Texture Image load
+  const textureLoader = new THREE.TextureLoader();
+  const texture = textureLoader.load(
+    "/textures/brick/Bricks_Terracotta_003_ambientOcclusion.jpg",
+    () => {
+      console.log("load complete");
+    },
+    () => {
+      console.log("loading");
+    },
+    () => {
+      console.log("load error");
+    }
+  );
+
+  // Renderer
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // Scene
+  const scene = new THREE.Scene();
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.y = 1.5;
+  camera.position.z = 4;
+  scene.add(camera);
+
+  // Light
+  const ambientLight = new THREE.AmbientLight("white", 0.5);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight("white", 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
+
+  // Controls
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  // Mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  //No highlight, no reflection
+  const materialLambert = new THREE.MeshBasicMaterial({
+    color: "orangered",
+    map: texture,
+  });
+
+  const mesh = new THREE.Mesh(geometry, materialLambert);
+  scene.add(mesh);
+
+  // 그리기
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const delta = clock.getDelta();
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener("resize", setSize);
+
+  draw();
+}
